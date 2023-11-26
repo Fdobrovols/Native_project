9eaeaa25f6b472c645c860c2346f680adb2a7a3b
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-11-26 23:49:13 +0200

    fix

--- src/components/Comments/CommentItem.jsx
+++ src/components/Comments/CommentItem.jsx
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
     avatar: {
         width: 28,
         height: 20,
-        borderRadius: 28,
+        borderRadius: 18,
     },
     commentWrap: {
         position: 'relative',
