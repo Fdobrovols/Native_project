ba046e2bdde56a4ff03a8862ec75155c0cf11aab
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-12-19 23:33:30 +0200

    fix

--- src/components/Comments/CommentItem.jsx
+++ src/components/Comments/CommentItem.jsx
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
     commentContainer: {
         display: 'flex',
         flexDirection: 'row',
-        marginBottom: 10,
+        marginBottom: 5,
         gap: 10,
         width: 20,
     },
