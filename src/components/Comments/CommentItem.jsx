25efc9251d008ac3e7ccc677ae1f4855eb053559
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-12-21 22:58:06 +0200

    fix gap

--- src/components/Comments/CommentItem.jsx
+++ src/components/Comments/CommentItem.jsx
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         marginBottom: 5,
-        gap: 10,
+        gap: 100,
         width: 20,
     },
     avatar: {
