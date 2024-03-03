28d6e56211ca84bfb7b0cbca01b49cdf281c33fc
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-03-03 23:27:17 +0200

    fix height

--- src/components/Buttons/Btn.jsx
+++ src/components/Buttons/Btn.jsx
@@ -15,7 +15,7 @@ export default function Btn({ text, onPress, disabled, style, styleTitle }) {
 
 const styles = StyleSheet.create({
     button: {
-        height: 5,
+        height: 4,
         backgroundColor: '#334c72',
         borderRadius: 30,
         marginTop: 40,
