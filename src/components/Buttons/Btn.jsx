6b6b8f43bb0b3a6b7cf4fb645857020caee79e35
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-03-18 23:12:33 +0200

    fix padding

--- src/components/Buttons/Btn.jsx
+++ src/components/Buttons/Btn.jsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
         borderRadius: 25,
         marginTop: 40,
         alignItems: 'center',
-        padding: 1,
+        padding: 2,
     },
     title: {
         fontFamily: 'Roboto-Regular',
