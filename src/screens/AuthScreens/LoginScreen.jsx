7109f1d3f88e28b13ed87dcb2a0eadbe290bb56e
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-03-29 22:44:14 +0200

    fix loginScreen color

--- src/screens/AuthScreens/LoginScreen.jsx
+++ src/screens/AuthScreens/LoginScreen.jsx
@@ -145,7 +145,7 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#29938e',
+        backgroundColor: '#236742',
         alignItems: 'center',
         justifyContent: 'flex-end',
     },
