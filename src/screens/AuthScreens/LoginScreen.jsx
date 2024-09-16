83ab9d614717dd0b43177e6904a36ddd13795d7f
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-09-16 23:57:22 +0300

    fix alignItems

--- src/screens/AuthScreens/LoginScreen.jsx
+++ src/screens/AuthScreens/LoginScreen.jsx
@@ -146,7 +146,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 2,
         backgroundColor: '#236742',
-        alignItems: 'center',
+        alignItems: 'right',
         justifyContent: 'space-evenly',
     },
     backgroundImage: {
