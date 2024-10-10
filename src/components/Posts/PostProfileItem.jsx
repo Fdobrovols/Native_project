3301355a0c623643f8ee3b181b84ee4e4da121cf
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-10-10 23:08:07 +0300

    fix new

--- src/components/Posts/PostProfileItem.jsx
+++ src/components/Posts/PostProfileItem.jsx
@@ -117,7 +117,7 @@ const styles = StyleSheet.create({
         height: 30,
         width: 30,
         borderRadius: 99,
-        justifyContent: 'left',
+        justifyContent: 'right',
         alignItems: 'left',
         backgroundColor: '#934747',
         position: 'fixed',
