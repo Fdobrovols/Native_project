72a59eaa5b3f53e99ad3fb345eb28beade44f0e9
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-10-19 22:00:45 +0300

    new fix

--- src/components/Posts/PostItem.jsx
+++ src/components/Posts/PostItem.jsx
@@ -131,7 +131,7 @@ const styles = StyleSheet.create({
     locationText: {
         fontFamily: 'Roboto-Regular',
         color: '#212121',
-        fontSize: 15,
+        fontSize: 14,
         textDecorationLine: 'underline',
     },
     postIcon: {
