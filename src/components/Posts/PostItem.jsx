6e598a5f36e636dc3109a1849587431a2c5915f4
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-10-21 21:53:48 +0300

    new fix

--- src/components/Posts/PostItem.jsx
+++ src/components/Posts/PostItem.jsx
@@ -109,7 +109,7 @@ const styles = StyleSheet.create({
     postDetails: {
         display: 'flex',
         flexDirection: 'row',
-        marginTop: 8,
+        marginTop: 7,
         justifyContent: 'space-between',
     },
     postComments: {
