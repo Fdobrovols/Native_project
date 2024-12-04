4a111c4b82fd21f94877f7031cc5de97ab3d7a96
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-12-04 22:20:56 +0200

    fix indicator

--- App.jsx
+++ App.jsx
@@ -45,7 +45,7 @@ export default function App()
     {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size="large" color="#0000ff" />
+                <ActivityIndicator size="large" color="#31317d" />
             </View>
         );
     }
