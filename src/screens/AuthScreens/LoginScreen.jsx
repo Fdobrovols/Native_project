4a1c8d0b6f170937c4836f4a939d050c3644eaa7
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2024-12-06 23:36:47 +0200

    fix new

--- src/screens/AuthScreens/LoginScreen.jsx
+++ src/screens/AuthScreens/LoginScreen.jsx
@@ -159,7 +159,7 @@ const styles = StyleSheet.create({
     form: {
         position: 'relative',
         paddingTop: 16,
-        paddingBottom: 20,
+        paddingBottom: 21,
         paddingHorizontal: 11,
         borderTopStartRadius: 15,
         borderTopEndRadius: 25,
