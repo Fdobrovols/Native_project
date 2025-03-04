98bc332ed1fd75a397ad9c8ccde7842cf54b42e7
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-03-04 20:49:09 +0200

    fix new

--- src/screens/AuthScreens/RegistrationScreen.jsx
+++ src/screens/AuthScreens/RegistrationScreen.jsx
@@ -350,7 +350,7 @@ const styles = StyleSheet.create({
     },
     input: {
         fontFamily: 'Roboto-Regular',
-        height: 51,
+        height: 5,
         borderRadius: 10,
         backgroundColor: '#4cad41',
         borderWidth: 3,
