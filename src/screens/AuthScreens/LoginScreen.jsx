9eb640d06b4c9ea3b6a33d53f93b807e9968d378
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-05-09 23:22:40 +0300

    x

--- src/screens/AuthScreens/LoginScreen.jsx
+++ src/screens/AuthScreens/LoginScreen.jsx
@@ -159,7 +159,7 @@ const styles = StyleSheet.create({
     form: {
         position: 'relative',
         paddingTop: 17,
-        paddingBottom: 21,
+        paddingBottom: 22,
         paddingHorizontal: 13,
         borderTopStartRadius: 17,
         borderTopEndRadius: 26,
