0e6f17385baa875b39fb34afac9e9dfb0595106f
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-05-19 21:56:04 +0300

    x

--- src/screens/AuthScreens/LoginScreen.jsx
+++ src/screens/AuthScreens/LoginScreen.jsx
@@ -165,7 +165,7 @@ const styles = StyleSheet.create({
         borderTopEndRadius: 26,
         backgroundColor: '#2883d9',
         fontFamily: 'sans-serif',
-        fontSize: 18,
+        fontSize: 19,
     },
     formTitle: {
         fontFamily: 'serif',
