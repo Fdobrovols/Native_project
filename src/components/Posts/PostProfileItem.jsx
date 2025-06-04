7c5d75e8d49a1c83b3bf4f38c5ce2807aaf9da36
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-06-04 22:26:49 +0300

    fix

--- src/components/Posts/PostProfileItem.jsx
+++ src/components/Posts/PostProfileItem.jsx
@@ -122,7 +122,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#334d63',
         position: 'fixed',
         top: 8,
-        right: 10,
+        right: 11,
         zIndex: 2,
     },
     postTitle: {
