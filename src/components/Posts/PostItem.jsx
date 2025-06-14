1659c5f4f1a04cf3294111bd214b0646fe3d1c3d
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-06-14 23:28:04 +0300

    x

--- src/components/Posts/PostItem.jsx
+++ src/components/Posts/PostItem.jsx
@@ -130,7 +130,7 @@ const styles = StyleSheet.create({
     },
     locationText: {
         fontFamily: 'Roboto-Regular',
-        color: '#212121',
+        color: '#3b71c2',
         fontSize: 14,
         textDecorationLine: 'revert',
     },
