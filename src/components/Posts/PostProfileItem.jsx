bbb562b69095b0a193b7aeda3f3320f800693ce6
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-07-29 23:05:05 +0300

    .

--- src/components/Posts/PostProfileItem.jsx
+++ src/components/Posts/PostProfileItem.jsx
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     },
     postPhotoWrap: {
         width: '100%',
-        height: 240,
+        height: 241,
         backgroundColor: '#f11e96',
         borderRadius: 7,
     },
