b8e4149f2549d507e429e5b184aa06b34b4ad4d6
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-08-19 21:10:41 +0300

    .

--- src/components/Posts/PostProfileItem.jsx
+++ src/components/Posts/PostProfileItem.jsx
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
         marginTop: 9,
         fontFamily: 'Roboto-Medium',
         color: '#6cda35',
-        fontSize: 17,
+        fontSize: 18,
     },
     postDetails: {
         display: 'flex',
