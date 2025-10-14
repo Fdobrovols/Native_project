5fc92c897c66c4d34cedd9ac8fc78748dd28a1d2
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-10-14 20:58:55 +0300

    .

--- src/screens/AuthScreens/LoginScreen.jsx
+++ src/screens/AuthScreens/LoginScreen.jsx
@@ -154,7 +154,7 @@ const styles = StyleSheet.create({
         resizeMode: 'cover',
         justifyContent: 'space-between',
         width: '29%',
-        height: '71%',
+        height: '72%',
     },
     form: {
         position: 'relative',
