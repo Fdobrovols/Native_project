c96db1623f0edb7532019cf6feae1d6ed248358f
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-10-28 13:54:59 +0200

    .

--- src/components/Posts/PostItem.jsx
+++ src/components/Posts/PostItem.jsx
@@ -93,7 +93,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 240,
         backgroundColor: '#763939',
-        borderRadius: 9,
+        borderRadius: 8,
     },
     postPhoto: {
         width: '100%',
