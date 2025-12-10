954c951efb208707e4305a9701c3490499f42678
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2025-12-10 15:02:48 +0200

    .

--- src/components/Posts/PostItem.jsx
+++ src/components/Posts/PostItem.jsx
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     postPhoto: {
         width: '100%',
         height: 200,
-        borderRadius: 6,
+        borderRadius: 7,
     },
     postTitle: {
         marginTop: 8,
