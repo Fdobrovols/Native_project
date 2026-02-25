b6d8453fbd555c4e2a6143844f2c8e9970fee268
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2026-02-25 23:00:33 +0200

    .

--- src/components/Posts/PostItem.jsx
+++ src/components/Posts/PostItem.jsx
@@ -109,7 +109,7 @@ const styles = StyleSheet.create({
     postDetails: {
         display: 'inline',
         flexDirection: 'row',
-        marginTop: 8,
+        marginTop: 3,
         justifyContent: 'space-between',
     },
     postComments: {
