5b3a37249b0fe72613f04f6e906db8dde0705375
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2026-08-09 21:55:33 +0300

    .

--- src/components/Comments/CommentItem.jsx
+++ src/components/Comments/CommentItem.jsx
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
         marginBottom: 2,
         fontFamily: 'cursive',
         color: '#428a70',
-        fontSize: 17,
+        fontSize: 12,
     },
     dateUser: {
         fontFamily: 'serif',
