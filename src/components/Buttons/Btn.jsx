2b060e58d26f8358fadf6c93c35a58777cec7b54
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2026-09-01 20:56:18 +0300

    .

--- src/components/Buttons/Btn.jsx
+++ src/components/Buttons/Btn.jsx
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     },
     title: {
         fontFamily: 'Roboto-Regular',
-        color: '#3fa446',
+        color: '#3f7ca4',
         fontSize: 14,
     },
 });
