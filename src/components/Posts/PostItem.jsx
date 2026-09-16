ad35a209adecdac252849dc03455eaf0bea53d61
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2026-09-16 22:16:39 +0300

    .

--- src/components/Posts/PostItem.jsx
+++ src/components/Posts/PostItem.jsx
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
     postComments: {
         display: 'flex',
         flexDirection: 'row',
-        gap: 2,
+        gap: 1,
     },
     postLocation: {
         marginLeft: 'auto',
